fae10b87d9c2b3b77f1b132c990698616b323b18
SOGO Haraguchi <harasou@pepabo.com>
2015-03-23 01:51:44 +0900

    リポジトリを Alfread のディレクトリにリスクするスクリプトを追加

---
+++ linkalfread.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
+LIB="$HOME/Library/Application Support/Alfred 2/Alfred.alfredpreferences/workflows"
+cd "$LIB" || exit 2
+
+for wf in GeoIP2 Munin PasswordPaster RunSSH ScreenCapture
+do
+    printf "%-15s ... " $wf 
+    if [ -e $wf ] ; then
+        echo skip.
+    else
+        ln -fs "$SCRIPT_DIR/$wf" $wf && echo ok || echo error
+    fi
+done

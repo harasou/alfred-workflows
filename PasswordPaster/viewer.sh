6d6af1c311a744dc84b0e15e73ea484a9cdf335c
SOGO Haraguchi <harasou@pepabo.com>
2015-03-22 14:16:51 +0900

    PasswordPaster: 複数のxmlを読み込めるように変更

--- PasswordPaster/viewer.sh
+++ PasswordPaster/viewer.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cachedir="$(cd `dirname $0`;pwd)/cache"; [ -d "$cachedir" ] || mkdir "$cachedir"
-
-         xml="$cachedir/filter.xml"
-history_file="$cachedir/history.xml"
+cachebase="$(cd $(dirname $0);pwd)/cache"; [ -d "$cachebase" ] || mkdir "$cachebase"
+historyfile="$cachebase/history.xml"
 
 addResult() {
     case $1 in
     C*)
-        echo "<item uid='$1' arg='$cachedir/$1.txt' valid='yes' autocomplete='$2' type='file'><title>Comment</title><subtitle>shiftキーを押してください - $1.txt</subtitle><icon>img/comment.png</icon><folder>$2</folder></item>"
+        echo "<item uid='$1' arg='$2' valid='yes' autocomplete='$3' type='file'><title>Comment</title><subtitle>shiftキーを押してください - $1.txt</subtitle><icon>img/comment.png</icon><folder>$3</folder></item>"
         ;;
     *)
         local value=$(echo "$7"|xmlencode)
@@ -18,9 +16,10 @@ addResult() {
 }
 
 idm2alfred(){
-    idm_xml="$1"
-
-    find "$cachedir" -maxdepth 1 -name "C*.txt" -exec rm -f {} \;
+    local xmlpath="$1"
+    local xmlname="$(basename ${xmlpath%.xml})"
+    local cachedir="$cachebase/$xmlname"; rm -rf "$cachedir" && mkdir "$cachedir" || return
+    local filterxml="$cachedir/filter.xml"
 
     folder="/"
     while read -r line
@@ -49,14 +48,14 @@ idm2alfred(){
             [[ $cmnt =~ \</comment\>$ ]] && { cflag=flase; cmnt=${cmnt%</comment>}; }
             [ -n "$cmnt" ] && echo "$cmnt" >> "$cachedir/C$inum.txt"
         elif [ "$line" == "</item>" ] ; then
-            addResult "I$inum" "$folder" "$name"       "$pfolder"    "next.png"      "$pfolder" "$password"
-            [ -n "$account"  ] && addResult "a$inum" "$folder" "Account"     "$folder"     "account.png"   "$folder" "$account"
-            [ -n "$password" ] && addResult "b$inum" "$folder" "Password"    "$folder"     "password.png"  "$folder" "$password"
-            [ -n "$item1"    ] && addResult "c$inum" "$folder" "${item1[0]}" "${item1[1]}" "tag_blue.png"  "$folder" "${item1[1]}"
-            [ -n "$item2"    ] && addResult "d$inum" "$folder" "${item2[0]}" "${item2[1]}" "tag_green.png" "$folder" "${item2[1]}"
-            [ -n "$url"      ] && addResult "e$inum" "$folder" "URL"         "${url[1]}"   "favorite.png"  "$folder" "${url[1]}"
-            [ -n "$email"    ] && addResult "f$inum" "$folder" "E-MAIL"      "${email[1]}" "mail.png"      "$folder" "${email[1]}"
-            [ -f "$cachedir/C$inum.txt" ] && addResult "C$inum" "$folder"
+            addResult "I${inum}_$xmlname" "$folder" "$name"       "$pfolder"    "next.png"      "$pfolder" "$password"
+            [ -n "$account"  ] && addResult "a${inum}_$xmlname" "$folder" "Account"     "$folder"     "account.png"   "$folder" "$account"
+            [ -n "$password" ] && addResult "b${inum}_$xmlname" "$folder" "Password"    "$folder"     "password.png"  "$folder" "$password"
+            [ -n "$item1"    ] && addResult "c${inum}_$xmlname" "$folder" "${item1[0]}" "${item1[1]}" "tag_blue.png"  "$folder" "${item1[1]}"
+            [ -n "$item2"    ] && addResult "d${inum}_$xmlname" "$folder" "${item2[0]}" "${item2[1]}" "tag_green.png" "$folder" "${item2[1]}"
+            [ -n "$url"      ] && addResult "e${inum}_$xmlname" "$folder" "URL"         "${url[1]}"   "favorite.png"  "$folder" "${url[1]}"
+            [ -n "$email"    ] && addResult "f${inum}_$xmlname" "$folder" "E-MAIL"      "${email[1]}" "mail.png"      "$folder" "${email[1]}"
+            [ -f "$cachedir/C$inum.txt" ] && addResult "C${inum}_$xmlname" "$cachedir/C$inum.txt" "$folder"
             folder="$pfolder"
             pfolder="${folder%/*/}/"
         elif [[ "$line" == "</folder>" ]] ; then
@@ -65,15 +64,16 @@ idm2alfred(){
         else
             [ "$cflag" == true ] && echo "$line" >> "$cachedir/C$inum.txt"
         fi
-    done < <(iconv -f SHIFT_JIS "$idm_xml" | tr -d '\r') > "$xml"
+    done < <(iconv -f SHIFT_JIS "$xmlpath" | tr -d '\r') > "$filterxml"
 
     echo "IDM → Alfred"
 }
 
 keepass2alfred(){
-    idm_xml="$1"
-
-    find "$cachedir" -maxdepth 1 -name "C*.txt" -exec rm -f {} \;
+    local xmlpath="$1"
+    local xmlname="$(basename ${xmlpath%.xml})"
+    local cachedir="$cachebase/$xmlname"; rm -rf "$cachedir" && mkdir "$cachedir" || return
+    local filterxml="$cachedir/filter.xml"
 
     folder="/"
     while read -r line
@@ -88,41 +88,42 @@ keepass2alfred(){
             name=$(echo $line|sed 's|^.*>\(.*\)</.*$|\1|')
             pfolder="$folder"
             folder="$pfolder$name/"
-            [ "$category" == group ] && addResult "G$gnum" "$folder" "$name" "$pfolder" "folder.png" "$pfolder" ""
+            [ "$category" == group ] && addResult "G${gnum}_$xmlname" "$folder" "$name" "$pfolder" "folder.png" "$pfolder" ""
         elif [[ "$line" =~ '<username>' ]] ; then username=$(echo $line|sed 's|^.*<username>\(.*\)</username>$|\1|'|xmldecode)
         elif [[ "$line" =~ '<password>' ]] ; then password=$(echo $line|sed 's|^.*<password>\(.*\)</password>$|\1|'|xmldecode)
         elif [[ "$line" =~ '<url>'      ]] ; then      url=$(echo $line|sed 's|^.*<url>\(.*\)</url>$|\1|'          |xmldecode)
         elif [[ "$line" =~ '<comment>'  ]] ; then  comment=$(echo $line|sed 's|^.*<comment>\(.*\)</comment>$|\1|'  |xmldecode)
             [ -n "$comment" ] && echo "$comment" > "$cachedir/C$inum.txt"
         elif [[ "$line" =~ '</entry>' ]] ; then
-            [ "$category" == entry ] && addResult "I$inum" "$folder" "$name"    "$pfolder" "next.png"     "$pfolder" "$password"  
-            [ -n "$username" ] && addResult "a$inum" "$folder" "Account"  "$folder" "account.png"  "$folder" "$username"
-            [ -n "$password" ] && addResult "b$inum" "$folder" "Password" "$folder" "password.png" "$folder" "$password"
-            [ -n "$username" -a -n "$password" ] && addResult "w$inum" "$folder" "Account &amp; Password" "$folder" "comments.png" "$folder" "${username}__TAB__${password}"
-            [ -n "$url"      ] && addResult "e$inum" "$folder" "URL"      "$url"    "favorite.png" "$folder" "$url" 
-            [ -n "$comment"  ] && addResult "C$inum" "$folder"
+            [ "$category" == entry ] && addResult "I${inum}_$xmlname" "$folder" "$name"    "$pfolder" "next.png"     "$pfolder" "$password"
+            [ -n "$username" ] && addResult "a${inum}_$xmlname" "$folder" "Account"  "$folder" "account.png"  "$folder" "$username"
+            [ -n "$password" ] && addResult "b${inum}_$xmlname" "$folder" "Password" "$folder" "password.png" "$folder" "$password"
+            [ -n "$username" -a -n "$password" ] && addResult "w${inum}_$xmlname" "$folder" "Account &amp; Password" "$folder" "comments.png" "$folder" "${username}__TAB__${password}"
+            [ -n "$url"      ] && addResult "e${inum}_$xmlname" "$folder" "URL"      "$url"    "favorite.png" "$folder" "$url"
+            [ -n "$comment"  ] && addResult "C${inum}_$xmlname" "$cachedir/C$inum.txt" "$folder"
             folder="$pfolder"
             pfolder="${folder%/*/}/"
         elif [[ "$line" =~ '</group>' ]] ; then
             folder="$pfolder"
             pfolder="${folder%/*/}/"
         fi
-    done < "$idm_xml" | iconv -f UTF8 -t UTF8-MAC > "$xml"
+    done < "$xmlpath" | iconv -f UTF8 -t UTF8-MAC > "$filterxml"
 
     echo "KeePass → Alfred"
 }
 
 listxml(){
-    input="$1"
+    local input="$1"
+    local xmls="$(ls $cachebase/*/filter.xml)"
 
     echo '<?xml version="1.0"?>'
     echo '<items>'
     if [ "$input" == "" ]; then
-        [ -f "$history_file" ] && cat "$history_file" || grep -F "<folder>/</folder>" "$xml"
+        [ -f "$historyfile" ] && cat "$historyfile" || grep -hF "<folder>/</folder>" $xmls
     elif [[ "$input" =~ ^/ ]]; then
-        grep -F "<folder>${input%/*}/</folder>" "$xml" | grep -i "<title>${input##*/}.*</title>"
+        grep -hF "<folder>${input%/*}/</folder>" $xmls | grep -i "<title>${input##*/}.*</title>"
     else
-        grep -i "<title>.*$input.*</title>" "$xml"
+        grep -hi "<title>.*$input.*</title>" $xmls
     fi
     echo '</items>'
 }
@@ -137,28 +138,29 @@ xmldecode(){
 
 putvalue(){
     local uid="$1"
+    local xmls="$(ls $cachebase/*/filter.xml)"
 
     [[ $uid =~ ^F ]] && return
     [[ $uid =~ ^C ]] && return
 
-    curr=$(grep    "uid='$uid'" "$xml")
-    hist=$(grep -v "uid='$uid'" "$history_file" | head -n8)
-    { echo -e "$curr\n$hist" ; } > "$history_file"
+    curr=$(grep -h "uid='$uid'" $xmls)
+    hist=$(grep -v "uid='$uid'" "$historyfile" | head -n8)
+    { echo -e "$curr\n$hist" ; } > "$historyfile"
 
     echo -n $(echo "$curr" | sed 's|^.*<value>\([^<]*\)</value>.*$|\1|' | xmldecode | sed 's|\\|\\\\|g')
 }
 
 rmhistory(){
     uid="$1"
-    sed -i "" "/uid='$uid'/d" "$history_file"
+    sed -i "" "/uid='$uid'/d" "$historyfile"
 }
 
 case "$1" in
 keepass2alfred) keepass2alfred "$2" ;;
     idm2alfred) idm2alfred     "$2" ;;
           list) listxml        "$2" ;;
-     rmhistory) rmhistory      "$2" ;;
          value) putvalue       "$2" ;;
+     rmhistory) rmhistory      "$2" ;;
 esac
 
 exit 0

09547f247e815233b1a6cc2ba8bd988867c62e50
mizuki.yokoo <yokoo@pepabo.com>
2018-04-24 16:24:29 +0900

    iconvの新しいバージョンではUTF MACが無いのと、必用なさそうなので変換を削除

--- PasswordPaster/viewer.sh
+++ PasswordPaster/viewer.sh
@@ -107,7 +107,7 @@ keepass2alfred(){
             folder="$pfolder"
             pfolder="${folder%/*/}/"
         fi
-    done < "$xmlpath" | iconv -f UTF8 -t UTF8-MAC > "$filterxml"
+    done < "$xmlpath" > "$filterxml"
 
     echo "KeePass → Alfred"
 }
